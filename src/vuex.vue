092f34ff29cb5cd4524db8ae02b0235e70752cd5
wwwjiahuan <465909954@qq.com>
2018-03-08 20:36:15 +0800

    1. state访问状态对象

--- src/vuex.vue
+++ src/vuex.vue
@@ -2,13 +2,28 @@
 <template>
   <div id="app">
       <h1>Hello World</h1>
-      <p>{{ $store.state.count}}</p>
+      <p>{{ $store.state.count}} - {{count}}</p>
       <button @click="$store.commit('jia')">+</button>
       <button @click="$store.commit('jian')">-</button>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
-  
+    //vuex 1. state访问状态对象v
+    name:'app',
+    // data(){
+    //     return {
+    //         count:0
+    //     }
+    // },
+    // computed:{
+    //     count(){
+    //         return this.$store.state.count+10
+    //     }
+    // }
+    computed:mapState([
+        "count"
+    ])
 }
 </script>
