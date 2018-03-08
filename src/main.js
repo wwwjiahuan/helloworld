a2165be90cae461046b95b4e66d771ea9e8c92bc
wwwjiahuan <465909954@qq.com>
2018-03-08 20:14:40 +0800

    13. 编程式导航

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Router from './Router'
+//14. vuex探秘
+import store from './store'
+import vuex from './vuex.vue'
+
+new Vue({
+    el:'#app',
+    store,
+    render:xx=>xx(vuex)
+})
+//import Router from './Router'
 // import transition from './transition.vue' 
 
 // new Vue({
---
+++ src/vuex.vue
@@ -0,0 +1,14 @@
+// 14. vuex探秘
+<template>
+  <div id="app">
+      <h1>Hello World</h1>
+      <p>{{ $store.state.count}}</p>
+      <button @click="$store.commit('jia')">+</button>
+      <button @click="$store.commit('jian')">-</button>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
